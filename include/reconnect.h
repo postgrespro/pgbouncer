4f0a300242a2a75de0e13c37cb5dd40841cb05f9
Constantin S. Pan <kvapen@gmail.com>
2016-06-22 16:19:33 +0300

    Add the forgotten bcc reconnect sources.

---
+++ include/reconnect.h
@@ -0,0 +1,6 @@
+#ifndef RECONNECT_H
+#define RECONNECT_H
+
+void reconnect_setup(void);
+
+#endif
---
+++ src/reconnect.c
@@ -0,0 +1,48 @@
+#include "bouncer.h"
+
+static struct event ev_reconnect;
+
+static void serverlist_reconnect_bccs(struct StatList *list)
+{
+	struct List *item;
+	statlist_for_each(item, list) {
+		int i;
+		PgSocket *server = container_of(item, PgSocket, head);
+
+		for (i = 0; i < server->sbuf.bcc_count; i++) {
+			SBuf *bcc = server->sbuf.bcc + i;
+			if (!bcc->sock) {
+				slog_warning(server, "reconnecting bcc #%d\n", i);
+				dns_connect(server);
+			}
+		}
+	}
+}
+
+#define RECONNECT_PERIOD 10
+
+static void reconnect_bccs(int s, short flags, void *arg)
+{
+	struct List *item;
+	struct timeval period = { RECONNECT_PERIOD, 0 };
+
+	log_warning("reconnect_bccs()");
+
+	statlist_for_each(item, &pool_list) {
+		PgPool *pool = container_of(item, PgPool, head);
+		serverlist_reconnect_bccs(&pool->active_server_list);
+		serverlist_reconnect_bccs(&pool->idle_server_list);
+		serverlist_reconnect_bccs(&pool->used_server_list);
+		serverlist_reconnect_bccs(&pool->tested_server_list);
+	}
+
+	safe_evtimer_add(&ev_reconnect, &period);
+}
+
+void reconnect_setup(void)
+{
+	struct timeval period = { RECONNECT_PERIOD, 0 };
+
+	evtimer_set(&ev_reconnect, reconnect_bccs, NULL);
+	safe_evtimer_add(&ev_reconnect, &period);
+}
